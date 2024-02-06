c5ec2810bdd41664a85f794e72aba6282d4d21aa
MuhammadUmerKhan <muhammadumerk546@gmail.com>
2024-02-06 21:30:55 +0500

    Create 6Feb2024.py

---
+++ All Files/6Feb2024.py
@@ -0,0 +1,13 @@
+# import pandas as pd
+
+my_dict = {"ID":[1, 2, 3,4 ,5 ,6 ,7 ,8 ,9 ,10],
+           "Name":['Umer', 'Abdullah', 'Farhan', "Ali", "Zaid",
+                   "Uzair", "Musab", "Osman", "Abu Bakar", "Kashan"],
+           "Age":[23, 45, 67, 87, 55, 65, 34, 32, 23, 43],
+           }
+# df = pd.DataFrame(my_dict)
+# df.head()
+# df.drop(columns='ID', inplace=True)
+print(my_dict)
+my_dict.keys()
+my_dict.items()
